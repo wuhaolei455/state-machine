913281906d8885babccf76594eea9299436ccbd2
wuhaolei <wuhaolei@kanyun.com>
2025-11-02 21:57:01 +0800

    fsm

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,112 @@
-import { Config, Options } from "./types";
+import { Actions, Config, EventTypes, Events, Options, StateEvents, Transition } from "./types";
+import { createEventEmitter, EventEmitter, EventHandler, EventListener } from 'holly-emitter';
 
 export function createMachine<T extends Config>(config: T, options: Options<T>) {
-  const { initialState } = options;
+  const { initialState, emitter = createEventEmitter<Events<T>>() } = options;
+  const stateEmitter = createEventEmitter<StateEvents<T>>();
+  const handlers = buildHandlers(config, stateEmitter);
+  const actions = buildActions(config, transition)
 
   let current = initialState;
 
+  /**
+   * 获取当前状态
+   */
   function getState() {
     return current;
   }
 
+  /**
+   * 注册进入状态时触发的回调
+   */
+  function onEnter(handler: (data: Events<T>[EventTypes.ON_ENTER]) => Promise<void> | void) {
+    emitter.on(EventTypes.ON_ENTER, handler);
 
-  // todo
-  let secEmitter;
-  let emitter;
+    return function offEnter() {
+      emitter.off(EventTypes.ON_ENTER, handler);
+    }
+  }
 
-  const transition = () => {}
-  const buildActions = (config: T, func: any) => {}
-  const buildHandlers = (config: T, emitter: any) => {}
-  const handlers = buildHandlers(config, secEmitter);
-  const actions = buildActions(config, transition)
+  /**
+   * 注册退出状态时触发的回调
+   */
+  function onExit(handler: (data: Events<T>[EventTypes.ON_EXIT]) => void) {
+    emitter.on(EventTypes.ON_EXIT, handler);
+
+    return function offExit() {
+      emitter.off(EventTypes.ON_EXIT, handler);
+    }
+  }
+
+  /**
+   * 状态迁移
+   */
+  async function transition(action: Actions<T>, meta?: unknown) {
+    await emitter.emit(EventTypes.ON_EXIT, { action, current, meta})
+    const last = current;
+    const currentAction = config[current][action];
+    if (!currentAction) {
+      return;
+    }
+    current = await currentAction(meta);
+    await emitter.emit(EventTypes.ON_ENTER, { action, current, last, meta});
+    await stateEmitter.emit(current, {
+      action,
+      current,
+      last: last as Exclude<keyof T, keyof T & string>,
+      meta,
+    });
+    return current;
+  }
+
+  function buildActions<T extends Config>(config: T, transition: Transition<T>) {
+    const actionSet = Object.values(config).reduce((acc, curr) => {
+      Object.keys(curr).forEach(key => {
+        acc.add(key as Actions<T>);
+      });
+      return acc;
+    }, new Set<Actions<T>>())
+
+    return Array.from(actionSet).reduce((acc, action) => {
+      acc[action] = function doTransition(meta?: unknown) {
+        return transition(action, meta);
+      }
+      return acc;
+    }, {} as Record<Actions<T>, (meta?: unknown) => Promise<string | undefined>>);
+  }
+
+  function buildHandlers<T extends Config>(
+    config: T,
+    emitter: EventEmitter<StateEvents<T>>
+  ) {
+    const keys = Object.keys(config) as (keyof T)[];
+  
+    return keys.reduce((acc, cur: any) => {
+      const first = cur.slice(0, 1);
+      const rest = cur.slice(1).split("");
+  
+      const k = `on${first.toUpperCase()}${rest.join("")}` as `on${Capitalize<
+        Extract<keyof T, string>
+      >}`;
+  
+      acc[k] = (handler: EventHandler<StateEvents<T>[keyof T]>) => {
+        emitter.on(cur, handler);
+  
+        return () => emitter.off(cur, handler);
+      };
+  
+      return acc;
+    }, <Record<`on${Capitalize<keyof T & string>}`, (handler: EventHandler<StateEvents<T>[keyof T]>) => () => void>>{});
+  }
 
   return {
-    state: initialState,
-    actions,
+    ...actions,
+    ...handlers,
+    onEnter,
+    onExit,
+    transition,
+    getState,
   };
 }
+
+export * from './types';
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import { EventEmitter } from 'holly-emitter';
+
 export type Config = {
   [key: string]: Record<string, (...args: any[]) => Promise<string> | string>;
 };
@@ -12,4 +14,38 @@ export type Actions<T> = MyExclude<NestedKeys<Config>, keyof T>;
 
 export type Options<T> = {
   initialState: keyof T & string;
+  emitter?: EventEmitter<Events<T>>;
 };
+
+export type Events<T> = {
+  onEnter: {
+    action: Actions<T>;
+    current: keyof T;
+    last: keyof T;
+    meta?: unknown
+  };
+  onExit: {
+    action: Actions<T>;
+    current: keyof T & string;
+    meta?: unknown
+  };
+}
+
+export type StateEvents<T> = {
+  [K in keyof T]: {
+    action: Actions<T>;
+    current: K;
+    last: Exclude<keyof T, K>;
+    meta?: unknown
+  };
+}
+
+export const enum EventTypes {
+  ON_ENTER = "onEnter",
+  ON_EXIT = "onExit",
+}
+
+export type Transition<T> = (
+  action: Actions<T>,
+  meta?: unknown
+) => Promise<(keyof T & string) | undefined>;
